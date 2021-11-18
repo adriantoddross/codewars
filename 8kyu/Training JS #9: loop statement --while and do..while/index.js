e52e33dd6a17f49174a484a5056beb184c2c7c3d
Adrian Ross <adriantoddross@gmail.com>
2021-11-18 09:17:22 -0500

    Solve training js #9 on 2nd attempt

--- 8kyu/Training JS #9: loop statement --while and do..while/index.js
+++ 8kyu/Training JS #9: loop statement --while and do..while/index.js
@@ -1,1 +1,13 @@
-// Didn't complete this one... had to check the solution!
+function padIt(str, n) {
+  let count = 1;
+  let paddedString = str;
+
+  while (count < n + 1) {
+    count % 2 === 0
+      ? (paddedString += "*")
+      : (paddedString = "*" + paddedString);
+    count++;
+  }
+
+  return paddedString;
+}
