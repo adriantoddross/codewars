bcad7e4351b0c938bc49164315fc4e4bd41e3dbc
Adrian Ross <adriantoddross@gmail.com>
2021-11-29 14:03:18 -0500

    Complete training js #13

---
+++ 8kyu/Training JS #13: Number object and its properties/index.js
@@ -0,0 +1,16 @@
+function whatNumberIsIt(n) {
+  if (isNaN(n)) return "Input number is Number.NaN";
+
+  switch (n) {
+    case Number.POSITIVE_INFINITY:
+      return "Input number is Number.POSITIVE_INFINITY";
+    case Number.NEGATIVE_INFINITY:
+      return "Input number is Number.NEGATIVE_INFINITY";
+    case Number.MAX_VALUE:
+      return "Input number is Number.MAX_VALUE";
+    case Number.MIN_VALUE:
+      return "Input number is Number.MIN_VALUE";
+    default:
+      return `Input number is ${n}`;
+  }
+}
